895d7903afdb2e90f5317816e089194389f6df00
1-10 Ooba RazerBlade <ooba@1-10.com>
2023-07-21 19:30:21 +0900

    [update] stop 削除

--- Runtime/AnimancerExtension.cs
+++ Runtime/AnimancerExtension.cs
@@ -7,56 +7,56 @@ namespace HRYooba.Library
 {
     public static class AnimancerExtension
     {
-        public static AnimancerState PlaySafety(this AnimancerComponent animancer, AnimationClip clip, Action onEnd = null)
+        public static AnimancerState Play(this AnimancerComponent animancer, AnimationClip clip, Action onEnd = null)
         {
             var state = animancer.Play(clip);
             state.Events.OnEnd += () =>
             {
                 onEnd?.Invoke();
-                state.Stop();
+                state.Events.Clear();
             };
             return state;
         }
 
-        public static AnimancerState PlaySafety(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, Action onEnd = null)
+        public static AnimancerState Play(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, Action onEnd = null)
         {
             var state = animancer.Play(clip, fadeDuration);
             state.Events.OnEnd += () =>
             {
                 onEnd?.Invoke();
-                state.Stop();
+                state.Events.Clear();
             };
             return state;
         }
 
-        public static AnimancerState PlaySafety(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, FadeMode fadeMode, Action onEnd = null)
+        public static AnimancerState Play(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, FadeMode fadeMode, Action onEnd = null)
         {
             var state = animancer.Play(clip, fadeDuration, fadeMode);
             state.Events.OnEnd += () =>
             {
                 onEnd?.Invoke();
-                state.Stop();
+                state.Events.Clear();
             };
             return state;
         }
 
         public static async UniTask<AnimancerState> PlayAsync(this AnimancerComponent animancer, AnimationClip clip, CancellationToken cancellationToken)
         {
-            var state = animancer.PlaySafety(clip);
+            var state = animancer.Play(clip);
             await state.WithCancellation(cancellationToken);
             return state;
         }
 
         public static async UniTask<AnimancerState> PlayAsync(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, CancellationToken cancellationToken)
         {
-            var state = animancer.PlaySafety(clip, fadeDuration);
+            var state = animancer.Play(clip, fadeDuration);
             await state.WithCancellation(cancellationToken);
             return state;
         }
 
         public static async UniTask<AnimancerState> PlayAsync(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, FadeMode fadeMode, CancellationToken cancellationToken)
         {
-            var state = animancer.PlaySafety(clip, fadeDuration, fadeMode);
+            var state = animancer.Play(clip, fadeDuration, fadeMode);
             await state.WithCancellation(cancellationToken);
             return state;
         }
