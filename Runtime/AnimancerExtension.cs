7a9d591e7f612a457be2d48b286be02cd243a86e
1-10 Ooba RazerBlade <ooba@1-10.com>
2023-08-01 20:30:50 +0900

    [update] pause playable追加

--- Runtime/AnimancerExtension.cs
+++ Runtime/AnimancerExtension.cs
@@ -8,24 +8,45 @@ namespace HRYooba.Library
 {
     public static class AnimancerExtension
     {
-        public static AnimancerState PlayOnce(this AnimancerComponent animancer, AnimationClip clip)
+        public static AnimancerState PlayOnce(this AnimancerComponent animancer, AnimationClip clip, bool pausePlayable = false)
         {
             var state = animancer.Play(clip);
+
             state.Events.OnEnd = () => state.IsPlaying = false;
+            if (pausePlayable)
+            {
+                animancer.Playable.UnpauseGraph();
+                state.Events.OnEnd += () => animancer.Playable.PauseGraph();
+            }
+
             return state;
         }
 
-        public static AnimancerState PlayOnce(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration)
+        public static AnimancerState PlayOnce(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, bool pausePlayable = false)
         {
             var state = animancer.Play(clip, fadeDuration);
+
             state.Events.OnEnd = () => state.IsPlaying = false;
+            if (pausePlayable)
+            {
+                animancer.Playable.UnpauseGraph();
+                state.Events.OnEnd += () => animancer.Playable.PauseGraph();
+            }
+
             return state;
         }
 
-        public static AnimancerState PlayOnce(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, FadeMode fadeMode)
+        public static AnimancerState PlayOnce(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, FadeMode fadeMode, bool pausePlayable = false)
         {
             var state = animancer.Play(clip, fadeDuration, fadeMode);
+
             state.Events.OnEnd = () => state.IsPlaying = false;
+            if (pausePlayable)
+            {
+                animancer.Playable.UnpauseGraph();
+                state.Events.OnEnd += () => animancer.Playable.PauseGraph();
+            }
+
             return state;
         }
 
@@ -50,23 +71,23 @@ namespace HRYooba.Library
             return state;
         }
 
-        public static async UniTask<AnimancerState> PlayOnceAsync(this AnimancerComponent animancer, AnimationClip clip, CancellationToken cancellationToken)
+        public static async UniTask<AnimancerState> PlayOnceAsync(this AnimancerComponent animancer, AnimationClip clip, CancellationToken cancellationToken, bool pausePlayable = false)
         {
-            var state = animancer.PlayOnce(clip);
+            var state = animancer.PlayOnce(clip, pausePlayable);
             await state.WithCancellation(cancellationToken);
             return state;
         }
 
-        public static async UniTask<AnimancerState> PlayOnceAsync(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, CancellationToken cancellationToken)
+        public static async UniTask<AnimancerState> PlayOnceAsync(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, CancellationToken cancellationToken, bool pausePlayable = false)
         {
-            var state = animancer.PlayOnce(clip, fadeDuration);
+            var state = animancer.PlayOnce(clip, fadeDuration, pausePlayable);
             await state.WithCancellation(cancellationToken);
             return state;
         }
 
-        public static async UniTask<AnimancerState> PlayOnceAsync(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, FadeMode fadeMode, CancellationToken cancellationToken)
+        public static async UniTask<AnimancerState> PlayOnceAsync(this AnimancerComponent animancer, AnimationClip clip, float fadeDuration, FadeMode fadeMode, CancellationToken cancellationToken, bool pausePlayable = false)
         {
-            var state = animancer.PlayOnce(clip, fadeDuration, fadeMode);
+            var state = animancer.PlayOnce(clip, fadeDuration, fadeMode, pausePlayable);
             await state.WithCancellation(cancellationToken);
             return state;
         }
